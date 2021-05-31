8d0530bf020eab2a62075a66a48d69d7028242de
Yegor Rybchenko <yegorrybchenko@gmail.com>
2021-05-31 23:29:11 +0300

    test(page stat): create page stat spec

---
+++ spec/layers/application/log/entities/page_stat_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'layers/application/log/entities/page_stat'
+
+RSpec.describe Application::Log::Entities::PageStat do
+  subject { described_class.new(path, ip) }
+  let(:path) { 'page/1' }
+  let(:ip) { '111.23.23.22' }
+
+  context 'when we create view count' do
+    it 'creates successfully' do
+      expect(subject.path).to eq path
+      expect(subject.ips).to eq([ip])
+    end
+  end
+end
