1f18f4345abfc994a9f47a11b3c1976ca4cf0c9f
Yegor Rybchenko <yegorrybchenko@gmail.com>
2021-06-01 00:25:11 +0300

    test(add page stat command): add tests

---
+++ spec/layers/domain/log/commands/add_page_stat_spec.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'layers/domain/log/commands/add_page_stat'
+
+RSpec.describe Domain::Log::Commands::AddPageStat do
+  subject { described_class.call(stat, page_stat) }
+  let(:page_stat) { build(:page_stat) }
+
+  context 'when stat is empty' do
+    let(:stat) { Domain::Log::Entities::Stat.new({}) }
+
+    it 'adds new page stat to stat' do
+      expected = {
+        page_stat.path => page_stat
+      }
+      expect(subject.get).to eq expected
+    end
+  end
+
+  context 'when stat is not empty' do
+    let(:stat) { Domain::Log::Entities::Stat.new(stat_hash) }
+    let(:stat_hash) { { page_stat2.path => page_stat2 } }
+
+    context 'and pathes are different' do
+      let(:page_stat2) { build(:page_stat, path: 'new_path') }
+
+      it 'adds new page stat to current' do
+        expected = {
+          page_stat.path => page_stat
+        }.merge(stat_hash)
+
+        expect(subject.get).to eq expected
+      end
+    end
+
+    context 'when pathes are the same' do
+      let(:new_ip) { '22.47.41.35' }
+      let(:page_stat2) { build(:page_stat, ip: new_ip) }
+
+      it 'creates new page stat' do
+        expected_page_stat = Domain::Log::Entities::PageStat.new(page_stat2.path, [page_stat.ip, page_stat2.ip])
+        expected = {
+          page_stat2.path => expected_page_stat
+        }
+
+        expect(subject.get).to eq expected
+      end
+    end
+  end
+end
--- spec/layers/domain/log/entities/page_stat_spec.rb
+++ spec/layers/domain/log/entities/page_stat_spec.rb
@@ -14,4 +14,55 @@ RSpec.describe Domain::Log::Entities::PageStat do
       expect(subject.ips).to eq([ip])
     end
   end
+
+  describe '#==' do
+    let(:page_stat1) { described_class.new(path1, ips1) }
+    let(:page_stat2) { described_class.new(path2, ips2) }
+
+    context 'when pathes are equal' do
+      let(:path1) { 'page/43' }
+      let(:path2) { 'page/43' }
+
+      context 'and ips are qeual' do
+        let(:ips1) { [ip] }
+        let(:ips2) { [ip] }
+
+        it 'equals' do
+          expect(page_stat1).to eq page_stat2
+        end
+      end
+
+      context 'and ips are not equal' do
+        let(:ips1) { [ip] }
+        let(:ips2) { [ip, '12.65.44.22'] }
+
+        it 'does not equal' do
+          expect(page_stat1).not_to eq page_stat2
+        end
+      end
+    end
+
+    context 'when pathes are not equal' do
+      let(:path1) { 'page/43' }
+      let(:path2) { 'page/44' }
+
+      context 'and ips are qeual' do
+        let(:ips1) { [ip] }
+        let(:ips2) { [ip] }
+
+        it 'does not equal' do
+          expect(page_stat1).not_to eq page_stat2
+        end
+      end
+
+      context 'and ips are not equal' do
+        let(:ips1) { [ip] }
+        let(:ips2) { [ip, '12.65.44.22'] }
+
+        it 'does not equal' do
+          expect(page_stat1).not_to eq page_stat2
+        end
+      end
+    end
+  end
 end
