80ea495f47e75f5bc412fc6a661a54d15e5713a9
Yegor Rybchenko <yegorrybchenko@gmail.com>
2021-06-01 03:20:57 +0300

    feat(log): add create page stat service

--- layers/domain/log/services/create_page_stat.rb
+++ layers/domain/log/services/create_page_stat.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 
+require 'layers/domain/log/values/page_stat'
+
 module Domain
   module Log
     module Services
       class CreatePageStat
-        def initialize(page_view)
+        class << self
+          def call(page_view)
+            Values::PageStat.new(page_view.path, [page_view.ip])
+          end
         end
       end
     end
