67f4ce4bbae7046a9d39f00fe74918f984936cf8
Yegor Rybchenko <yegorrybchenko@gmail.com>
2021-06-01 03:59:27 +0300

    test(log): add tests for file reader

--- spec/layers/application/log/services/file_reader_spec.rb
+++ spec/layers/application/log/services/file_reader_spec.rb
@@ -4,6 +4,7 @@ require 'spec_helper'
 require 'layers/application/log/services/file_reader'
 
 RSpec.describe Application::Log::Services::FileReader do
+  subject { Application::Log::Services::FileReader.call(file) { |l| } }
   context 'when file exist' do
     context 'when file contains strings' do
       let(:text) { "first\nsecond\nlast\n" }
@@ -14,6 +15,14 @@ RSpec.describe Application::Log::Services::FileReader do
         Application::Log::Services::FileReader.call(file) { |line| result << line }
 
         expect(result).to eq text.split(/(?<=\n)/)
+      end
+
+      it 'closes file' do
+        allow(file).to receive(:close)
+
+        subject
+
+        expect(file).to receive(:close)
       end
     end
   end
