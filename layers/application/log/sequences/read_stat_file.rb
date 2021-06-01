b014599fb37ebb058a1f1271a4c4d879d9e1b00e
Yegor Rybchenko <yegorrybchenko@gmail.com>
2021-06-01 03:54:59 +0300

    feat(log): add read stat file service

--- layers/application/log/sequences/read_stat_file.rb
+++ layers/application/log/sequences/read_stat_file.rb
@@ -1,9 +1,37 @@
 # frozen_string_literal: true
 
+require 'layers/application/log/services/file_reader'
+require 'layers/application/log/serializers/page_views/string'
+require 'layers/domain/log/entities/stat'
+require 'layers/domain/log/services/create_page_stat'
+require 'layers/domain/log/commands/add_page_stat'
+
+require 'pry'
+
 module Application
   module Log
     module Sequences
       class ReadStatFile
+        def initialize(file_path, reader = Services::FileReader, serializer = Serializers::PageViews::String)
+          @file_path = file_path
+          @reader = reader
+          @serializer = serializer
+          @stat = Domain::Log::Entities::Stat.new({})
+        end
+
+        def call
+          reader.call(file_path) do |line|
+            page_view = serializer.new(line).call
+            page_stat = Domain::Log::Services::CreatePageStat.call(page_view)
+            Domain::Log::Commands::AddPageStat.new(stat, page_stat).call
+          end
+
+          stat
+        end
+
+        private
+
+        attr_reader :file_path, :reader, :serializer, :stat
       end
     end
   end
--- spec/layers/application/log/sequences/read_stat_file_spec.rb
+++ spec/layers/application/log/sequences/read_stat_file_spec.rb
@@ -4,37 +4,37 @@ require 'spec_helper'
 require 'layers/application/log/sequences/read_stat_file'
 
 RSpec.describe Application::Log::Sequences::ReadStatFile do
-  subject { described_class.new(file_path, reader, serializer).call }
-  let(:reader) { double }
-  let(:serializer) { double }
+  subject { described_class.new(file_path, reader, serializer_class).call }
+  let(:reader) { double(:reader) }
+  let(:serializer_class) { double(:serializer_class) }
+  let(:serializer1) { double(:serializer1) }
+  let(:serializer2) { double(:serializer2) }
   let(:string) { 'sring' }
   let(:string2) { 'sring2' }
-  let(:page_view) { Domain::Log::Values::PageView.new('path', 'ip') }
+  let(:page_view1) { Domain::Log::Values::PageView.new('path', 'ip') }
   let(:page_view2) { Domain::Log::Values::PageView.new('path2', 'ip2') }
   let(:page_stat1) { Domain::Log::Values::PageStat.new('path', ['ip']) }
   let(:page_stat2) { Domain::Log::Values::PageStat.new('path2', ['ip2']) }
   let(:file_path) { 'path_to_file' }
 
   before(:each) do
-    allow(reader).to receive(:call).with(file_path) do |&block|
-      block.call
-      string
-    end
+    allow(reader).to receive(:call).with(file_path).and_yield(string).and_yield(string2)
 
-    allow(serializer).to receive(:call).with(string).and_return(page_view)
-    allow(serializer).to receive(:call).with(string2).and_return(page_view2)
+    allow(serializer_class).to receive(:new).with(string).and_return(serializer1)
+    allow(serializer_class).to receive(:new).with(string2).and_return(serializer2)
+    allow(serializer1).to receive(:call).and_return(page_view1)
+    allow(serializer2).to receive(:call).and_return(page_view2)
   end
 
   it 'calls reader with file_path' do
     expect(reader).to receive(:call).with(file_path)
-    expect(reader).to yield_with_no_args
 
     subject
   end
 
   it 'calls serializer with returned strings' do
-    expect(serializer).to receive(:call).with(string)
-    expect(serializer).to receive(:call).with(string2)
+    expect(serializer_class).to receive(:new).with(string)
+    expect(serializer_class).to receive(:new).with(string2)
 
     subject
   end
