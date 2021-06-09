97a229e8d729898430e0dee31037fd3d7389c916
Yegor Rybchenko <yegorrybchenko@gmail.com>
2021-06-10 02:39:10 +0300

    test: add specs for file to sorted stats

---
+++ spec/application/sequences/file_to_sorted_stats_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'application/sequences/file_to_sorted_stats'
+require 'application/sequences/file_to_stats'
+require 'domain/services/stat_collector'
+require 'domain/services/stat_count_sorter'
+
+RSpec.describe Application::Sequences::FileToSortedStats do
+  let(:file) { double }
+  let(:stats) { double }
+  let(:stats) { double }
+  let(:collected_stats) { double }
+  let(:sorted_stats) { double }
+
+  before(:each) do
+    allow(Application::Sequences::FileToStats).to receive_message_chain(:new, :call).with(file).with(no_args).and_return(stats)
+    allow(Domain::Services::StatCollector).to receive_message_chain(:new, :call).with(file).with(no_args).and_return(collected_stats)
+    allow(Domain::Services::StatCountSorter).to receive(:call).with(collected_stats, desc: true).and_return(sorted_stats)
+  end
+  
+  it 'returns sorted stats' do
+    expect(described_class.call(file)).to eq sorted_stats
+  end
+end
