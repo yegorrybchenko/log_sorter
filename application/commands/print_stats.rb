932a47fed43e13614f01ec0202adb21cb8dd5c23
Yegor Rybchenko <yegorrybchenko@gmail.com>
2021-06-10 02:59:37 +0300

    feat(Print Stats): adapt for collected stats

--- application/commands/print_stats.rb
+++ application/commands/print_stats.rb
@@ -4,16 +4,25 @@ module Application
   module Commands
     class PrintStats
       class << self
-        def call(stats, out = $stdout)
-          stats.each do |stat|
-            out.puts format(stat)
-          end
+        def call(collected_stats, out = $stdout)
+          print_total(collected_stats.total_views, out)
+          print_unique(collected_stats.unique_views, out)
         end
 
         private
 
-        def format(stat)
-          "#{stat.path} #{stat.ips.size} unique view(s)"
+        def print_total(total, out)
+          out.puts 'Most viewed pages:'
+          total.each do |stat|
+            out.puts "#{stat[0]} has #{stat[1]} view(s)"
+          end
+        end
+
+        def print_unique(unique, out)
+          out.puts "\nPages with most unique views:"
+          unique.each do |stat|
+            out.puts "#{stat[0]} #{stat[1]} unique view(s)"
+          end
         end
       end
     end
