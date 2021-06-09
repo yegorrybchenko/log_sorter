d8523dbb238f014bfaffc9a517806960a4af1dcb
Yegor Rybchenko <yegorrybchenko@gmail.com>
2021-06-09 19:20:13 +0300

    test(File reader): change to Page view file reader

--- spec/application/services/page_view_file_reader_spec.rb
+++ spec/application/services/page_view_file_reader_spec.rb
@@ -2,6 +2,7 @@
 
 require 'spec_helper'
 require 'application/services/file_reader'
+require 'application/deserializers/page_views/string'
 require 'domain/values/page_view'
 
 RSpec.describe Application::Services::PageViewFileReader do
@@ -10,19 +11,29 @@ RSpec.describe Application::Services::PageViewFileReader do
     context 'when file contains strings' do
       let(:text) { "first\nsecond\nlast\n" }
       let(:file) { StringIO.new(text) }
+      let(:page_view) { Domain::Values::PageView.new('path', 123) }
+
+      before(:each) do
+        %w(first second last).each { |l| stub_string_deserializer(l, page_view) }
+      end
 
       it 'passes to block page view string' do
         result = []
         described_class.call(file) { |page_view| result << page_view }
 
         result.each do |e|
-          exepct(e).to be_an_instance_of(Domain::Values::PageView)
+          expect(e).to be_an_instance_of(Domain::Values::PageView)
         end
       end
 
       it 'closes file' do
         expect(file).to receive(:close)
         subject
+      end
+
+      def stub_string_deserializer(string, return_value)
+        allow(Application::Deserializers::PageViews::String)
+          .to receive_message_chain(:new, :call).with(string).with(no_args).and_return(return_value)
       end
     end
   end
