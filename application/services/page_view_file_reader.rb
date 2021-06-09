5499b60514778948dd841d8c4f7e48ab128f3741
Yegor Rybchenko <yegorrybchenko@gmail.com>
2021-06-09 19:24:21 +0300

    feat(File reader): change to page view file reader

--- application/services/page_view_file_reader.rb
+++ application/services/page_view_file_reader.rb
@@ -2,10 +2,13 @@
 
 module Application
   module Services
-    class FileReader
+    class PageViewFileReader
       class << self
         def call(file, &block)
-          file.each(&block)
+          file.each do |line|
+            page_view = Deserializers::PageViews::String.new(line).call
+            block.call(page_view)
+          end
           file.close
         end
       end
