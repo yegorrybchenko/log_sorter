238412529526c83d3b701c282d1c4136d05d4398
Yegor Rybchenko <yegorrybchenko@gmail.com>
2021-06-09 19:57:15 +0300

    feat(Page stat): collect count of ips

--- spec/domain/values/page_stat_spec.rb
+++ spec/domain/values/page_stat_spec.rb
@@ -11,68 +11,7 @@ RSpec.describe Domain::Values::PageStat do
   context 'when we create view count' do
     it 'creates successfully' do
       expect(subject.path).to eq path
-      expect(subject.ips).to eq(Set.new([ip]))
-    end
-  end
-
-  describe '#==' do
-    let(:page_stat1) { described_class.new(path1, ips1) }
-    let(:page_stat2) { described_class.new(path2, ips2) }
-
-    context 'when pathes are equal' do
-      let(:path1) { 'page/43' }
-      let(:path2) { 'page/43' }
-
-      context 'and ips are qeual' do
-        let(:ips1) { [ip] }
-        let(:ips2) { [ip] }
-
-        it 'equals' do
-          expect(page_stat1).to eq page_stat2
-        end
-
-        context 'and ips are not sorted' do
-          let(:ip2) { '55.25.24.83' }
-          let(:ips1) { [ip, ip2] }
-          let(:ips2) { [ip2, ip] }
-
-          it 'equals' do
-            expect(page_stat1).to eq page_stat2
-          end
-        end
-      end
-
-      context 'and ips are not equal' do
-        let(:ips1) { [ip] }
-        let(:ips2) { [ip, '12.65.44.22'] }
-
-        it 'does not equal' do
-          expect(page_stat1).not_to eq page_stat2
-        end
-      end
-    end
-
-    context 'when pathes are not equal' do
-      let(:path1) { 'page/43' }
-      let(:path2) { 'page/44' }
-
-      context 'and ips are qeual' do
-        let(:ips1) { [ip] }
-        let(:ips2) { [ip] }
-
-        it 'does not equal' do
-          expect(page_stat1).not_to eq page_stat2
-        end
-      end
-
-      context 'and ips are not equal' do
-        let(:ips1) { [ip] }
-        let(:ips2) { [ip, '12.65.44.22'] }
-
-        it 'does not equal' do
-          expect(page_stat1).not_to eq page_stat2
-        end
-      end
+      expect(subject.ips).to eq({ ip => 1 })
     end
   end
 end
