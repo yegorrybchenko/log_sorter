fcb17f089d3887bccb58f313d8765f2d1ee0e6e0
Yegor Rybchenko <yegorrybchenko@gmail.com>
2021-06-10 02:54:59 +0300

    test: change tests for print stats command

--- spec/application/commands/print_stats_spec.rb
+++ spec/application/commands/print_stats_spec.rb
@@ -2,18 +2,24 @@
 
 require 'spec_helper'
 require 'application/commands/print_stats'
+require 'domain/values/collected_stat'
 
 RSpec.describe Application::Commands::PrintStats do
-  subject { described_class.call(stats, out) }
+  subject { described_class.call(collected_stats, out) }
   let(:out) { double(:out) }
-  let(:stats) { [page_stat1, page_stat2] }
-  let(:page_stat1) { Domain::Values::PageStat.new('/page/2', %w[ip1 ip2]) }
-  let(:page_stat2) { Domain::Values::PageStat.new('/page/3', ['ip3']) }
+  let(:collected_stats) { Domain::Values::CollectedStat.new(
+    [['page/1', 2], ['page/33', 4]],
+    [['page/f', 54], ['page12', 56]]
+    ) }
 
   it 'puts each stat to out' do
-    expect(out).to receive(:puts).with('/page/2 2 unique view(s)')
-    expect(out).to receive(:puts).with('/page/3 1 unique view(s)')
+    expect(out).to receive(:puts).with('Most viewed pages:')
+    expect(out).to receive(:puts).with('page/1 has 2 view(s)')
+    expect(out).to receive(:puts).with('page/33 has 4 view(s)')
 
+    expect(out).to receive(:puts).with("\nPages with most unique views:")
+    expect(out).to receive(:puts).with("page/f 54 unique view(s)")
+    expect(out).to receive(:puts).with("page12 56 unique view(s)")
     subject
   end
 end
